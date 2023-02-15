5d55ad9cb92e0b46706407f7ec1478c3865d857e
dhanush <dhanushdev201@gmail.com>
2023-02-15 12:49:57 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>punith is a superstar</h1>
+     <h1>punith is a super</h1>
      <div className='po'>
      <img src="https://photosfile.com/wp-content/uploads/2022/03/Cool-DP-10.jpeg" alt=""/>
      </div>
